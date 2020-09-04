1daa5f8a1397a5c731a78f7533526c1e81bd6a69
Cheriele Mordido <eyelmordido@gmail.com>
2020-09-03 21:09:03 -0400

    working api request

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/header.css';
 
 const Header = () => {
   return (
     <header>
-      <h1>The Shoppies</h1>
+      <h1 className='pageTitle'>The Shoppies</h1>
       <p>Nominate your favourite movies!</p>
     </header>
   );
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 const Results = () => {
+  const [term, setTerm] = useState('little women');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+   const getMovies = async () => {
+    try {
+      const response = axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}`, {
+        params: {
+          s: term,
+          type: 'movie',
+          r: 'json',
+          page: 1
+        }
+      });
+
+      console.log(response);
+
+    } catch (error) {
+      console.log(error);
+      
+    };
+   }; 
+
+   getMovies();
+  });
+
   return (
     <>
       <SearchBar />
+      <h2>Results for ""</h2>
       <ul>
         <li>Results</li>
         <li>Results</li>
