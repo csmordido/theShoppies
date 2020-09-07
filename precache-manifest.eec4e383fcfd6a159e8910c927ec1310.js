7e00ec86cf8556928f30432168a40aab97ce303c
Cheriele Mordido <eyelmordido@gmail.com>
2020-09-07 12:30:37 -0400

    Updates

---
+++ precache-manifest.eec4e383fcfd6a159e8910c927ec1310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fdaa6ab2817874cc2f6d1c98a3becc",
+    "url": "/theShoppies/index.html"
+  },
+  {
+    "revision": "e0bdb1a7cb40dc292adc",
+    "url": "/theShoppies/static/css/main.892cbc2b.chunk.css"
+  },
+  {
+    "revision": "2cfe05224bd69c30f790",
+    "url": "/theShoppies/static/js/2.5752ca79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/theShoppies/static/js/2.5752ca79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bdb1a7cb40dc292adc",
+    "url": "/theShoppies/static/js/main.77d19e24.chunk.js"
+  },
+  {
+    "revision": "7297c4875363729728c3",
+    "url": "/theShoppies/static/js/runtime-main.d4fede78.js"
+  }
+]);
